5ca1a59e25366fb46a8da52522a26d1c4d5cca4f
Joaosilva07 <joao.silva07@sempreceub.com>
2024-08-13 10:37:06 -0300

    Feat: Added Cpf validation

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-let dom = document;
+
+
+
+function submeter() {
+    let nome = document.getElementById('nome').value;
+    console.log(nome);
+    let idade = document.getElementById('idade').value;
+    console.log(idade);
+    let cpf = document.getElementById('cpf').value;
+    
+    
+    console.log(validarCpf(cpf));
+    
+    
+}
+
+
+function validarCpf(){
+    let cpf = document.getElementById('cpf').value;
+    
+    
+    if ( cpf == ""){
+        alert("cpf inválido")
+        return false;
+          
+    }
+
+    cpf = cpf.trim();
+    
+    if (/[a-zA-Z]/.test(cpf)) {
+        alert("NAO PODE LETRAS NO CPF AAAAAAAAAAAAAAAAA");   
+        console.log("tem letras");
+        return false;
+    }
+
+    if (!/^[\d.-]+$/.test(cpf)){
+        alert("SÓ PODER TER NUMEROS,'.' E '-' NO CPFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+        return false;
+        
+    }
+
+    // TO DO:  More validations
+    
+    return true;
+}
+
