0f72d40d0e3a98243c1f8c1d888f8d2707d5472a
Joaosilva07 <joao.silva07@sempreceub.com>
2024-08-16 08:56:53 -0300

    Feat: Cpf Max Length And valditation Added

--- script.js
+++ script.js
@@ -1,49 +1,25 @@
-
-
-
 function submeter() {
     let nome = document.getElementById('nome').value;
     console.log(nome);
     let idade = document.getElementById('idade').value;
     console.log(idade);
-    let cpf = document.getElementById('cpf').value;
-    
-    
-    console.log(validarCpf(cpf));
-    
-    
+    let cpfInput = document.getElementById('cpf');
+    let cpf = cpfInput.value;
+    cpf = cpf.replace(/\D/g, ''); // Remove non-digits
+
+    if (cpf.length !== 11) {
+        cpfInput.value = "cpf inválido";
+        return;
+    }
+
+    console.log(cpf);
 }
 
-
-function validarCpf(){
-    let cpf = document.getElementById('cpf').value;
-    
-    
-    if ( cpf == ""){
-        alert("cpf inválido")
-        return false;
-          
-    }
-
-    cpf = cpf.trim();
-    
-    if (/[a-zA-Z]/.test(cpf)) {
-        alert("NAO PODE LETRAS NO CPF AAAAAAAAAAAAAAAAA");   
-        console.log("tem letras");
-        return false;
-    }
-
-    if (!/^[\d.-]+$/.test(cpf)){
-        alert("SÓ PODER TER NUMEROS,'.' E '-' NO CPFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
-        return false;
-        
-    }
-
-    // TO DO:  More validations
-    // VALID CPF FORMATS:
-    // 000.000.000-00
-    // 00000000000
-    
-    return true;
+function mascaraCpf(input) {
+    let cpf = input.value;
+    cpf = cpf.replace(/\D/g, ''); // Remove non-digits
+    cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+    input.value = cpf;
 }
-
